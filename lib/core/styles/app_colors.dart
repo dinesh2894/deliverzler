a199fd292ff79a2e6196644a366e42ee5fc7bdc9
Dinesh Saini <sainidinesh2894@gmail.com>
2023-12-14 06:11:16 +0530

    Add App_styles.dart file

---
+++ lib/core/styles/app_colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Main
+  static const Color lightThemePrimary = Color(0xFFC11718);
+  static const Color darkThemePrimary = Color(0xFFC11718);
+  static const Color accentColor = Color(0xFF4b98db);
+
+  // Screen
+  static const Color lightThemeStatusBarColor = Color(0xFFFAFAFA);
+  static const Color darkThemeStatusBarColor = Color(0xFF303030);
+  static const Color lightThemeScaffoldBGColor = Color(0xFFFAFAFA);
+  static const Color darkThemeScaffoldBGColor = Color(0xFF303030);
+  static const Color lightThemeMajorBGColor = Color(0xFFFFFFFF);
+  static const Color darkThemeMajorBGColor = Color(0xFF212327);
+
+  // Text
+  static const Color lightThemeBigTextColor = Color(0xFF000000);
+}
