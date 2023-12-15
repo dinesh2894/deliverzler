bca96cc78d1613e0908d8f36d1453df103882be6
Dinesh Saini <sainidinesh2894@gmail.com>
2023-12-15 08:16:02 +0530

    Add new app_images.dart

---
+++ lib/core/styles/app_images.dart
@@ -0,0 +1,10 @@
+class AppImages {
+  static const String jsonPath = 'assets/json/';
+  static const String welcomeImagesPath = 'assets/images/welcome/';
+  static const String loginImagesPath = 'assets/images/login/';
+  static const String coreImagesPath = 'asstes/images/core/';
+  static const String dialogWidgetIconsPath = '${coreImagesPath}dialog_widget_icons/';
+
+  static const String appIconsPath = 'assets/images/app_icons/';
+  static const String appImagesPath = 'assets/images/app_images/';
+}
