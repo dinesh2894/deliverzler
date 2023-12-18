47fb6dd61ecf8fa03c7bf4c0200a8e16104c93c6
Dinesh Saini <sainidinesh2894@gmail.com>
2023-12-18 07:27:47 +0530

    add some todos

--- lib/auth/components/app_logo_component.dart
+++ lib/auth/components/app_logo_component.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 
 class AppLogoComponent extends StatelessWidget {
+  const AppLogoComponent({super.key});
+
   @override
   Widget build(BuildContext context) {
     return const Column(
       children: [
+        // Top Padding
         SizedBox(height: 100),
+        // TODO: add new Image here
+        // TODO: refresh() here
+        // TODO: reload() here
+        // TODO:
       ],
     );
   }
